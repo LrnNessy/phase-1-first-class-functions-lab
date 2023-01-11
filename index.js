4601a64b18cd903c3a0e0fcad60541e8f9727f4a
Lauren Shaughnessy <lrn.nessy@gmail.com>
2023-01-11 18:55:15 -0500

    commit tree 64c27472e3dae3b34c66379ebe59c42e41006ea8

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers){
+  const firstTwo = drivers.slice(0, 2);
+  console.log(firstTwo);
+  return firstTwo;
+}
+
+function returnLastTwoDrivers(drivers){
+  const lastTwo = drivers.slice(-2);
+  console.log(lastTwo);
+  return lastTwo;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  }
+  }
+  
+
+
+function fareDoubler(fare){ 
+  const total = fare * 2;
+ return total; 
+}
+
+
+function fareTripler(fare){
+  const total = fare * 3;
+  return total; 
+}
+
+
+function selectDifferentDrivers(names, callBack){
+return callBack(names);
+
+}
